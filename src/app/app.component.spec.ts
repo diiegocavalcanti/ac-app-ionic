857737cc523b77b56633665d506c5d2bf95f65bf
Silva <walteann.costs.silva@accenture.com>
2017-11-21 11:54:08 -0200

    teste do app componente

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,45 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By, BrowserModule } from "@angular/platform-browser";
+import { DebugElement } from "@angular/core";
+import { MyApp } from "./app.component";
+import { IonicModule } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
+describe("descreve o teste do appComponent", () => {
 
-describe('Era um componente Rafael', () => {
+	let myAppCom: MyApp;
+	let fixture: ComponentFixture<MyApp>;
+	let debugEl: DebugElement;
+	let elemento: HTMLElement;
 
+	beforeEach(	() =>	{
+		TestBed.configureTestingModule({
+            declarations: [ MyApp ],
+            imports: [
+                BrowserModule,
+                IonicModule.forRoot(MyApp)
 
+              ],
 
+              providers: [ StatusBar, SplashScreen ]
+		});
 
-  it('era um componente commit de rafa', () => {
-    expect(true).toBe(true);
+		fixture = TestBed.createComponent(MyApp);
+
+		myAppCom = fixture.componentInstance;
+
+        debugEl = fixture.debugElement.query(By.css('ion-title'));
+
+        elemento = debugEl.nativeElement;
+
+	});
+
+  it("Deveria o titulo esta com o texto 'MENU' ", () => {
+
+    expect(elemento.textContent).toContain('Menu');
+
   });
-
 
 
 });
